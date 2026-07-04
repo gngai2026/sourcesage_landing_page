716a8e13e6461f11e74ca3b7ba9636b69afe2869
Klaus <nggimseng@gmail.com>
2026-07-04 12:15:02 +0100

    Update WhatsApp number to Malaysian number (+60 12-515 1323)

--- components/WhatsappCTA.jsx
+++ components/WhatsappCTA.jsx
@@ -1,6 +1,6 @@
 'use client'
 
-const WA_NUMBER = '447546185317'
+const WA_NUMBER = '60125151323'
 const DEFAULT_MESSAGE = 'Hi, I need help sourcing a spare part. Brand: ___ Model: ___ Part needed: ___'
 const SIGNATURE = '\n\nPowered by SourceSage AI'
 
